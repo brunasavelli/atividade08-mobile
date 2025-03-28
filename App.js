cfc478ddbbcbef2b8f8c94ddb904d323a1c1e5a4
Bruna Savelli <bnsavelli@gmail.com>
2025-03-28 20:56:22 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from "./components/Header";
+import Footer from "./components/Footer"
+import Cards from "./components/Cards"
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <Cards />
+      <Cards />
+      <Footer />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 20,
+  },
+});
---
+++ components/Cards.js
@@ -0,0 +1,10 @@
+import styles from "../styles/Cards.module.css";
+
+export default function Cards() {
+    return (
+        <div className={styles.card}>
+            <h2>Title</h2>
+            <p>Description</p>
+        </div>
+    )
+}
