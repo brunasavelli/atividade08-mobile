4f612c3e930241f4eabb54d8e5e6f8dc93bff1d2
Bruna Savelli <bruna.savelli@aluno.senai.br>
2025-04-01 13:42:52 -0300

    finished

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F9F1F0',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
